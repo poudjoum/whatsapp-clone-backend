180cfe8d3e8415b33237f756119e0e869ffb4717
Jumpy <poudjoumrodrigue@gmail.com>
2024-07-07 11:38:15 +0100

    initialisation du projet 2 whatsapp clone2

---
+++ src/main/java/com/jumpy/tech/whatsappclone/shared/service/StatusNotification.java
@@ -0,0 +1,5 @@
+package com.jumpy.tech.whatsappclone.shared.service;
+
+public enum StatusNotification {
+    OK, ERROR, UNAUTHORIZED;
+}
---
+++ src/test/java/com/jumpy/tech/whatsappclone/WhatsCloseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumpy.tech.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsCloseBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
